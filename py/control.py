e1e1d22a9b6c438f401e492e7bf300f133a26444
Zhiming Wang <zmwangx@gmail.com>
2015-03-02 02:11:47 -0800

    control.py: add control code for lock-in

--- py/control.py
+++ py/control.py
@@ -6,15 +6,17 @@ Functions:
 ps_monitor_current: monitor and save current
 """
 
+from __future__ import division
 from __future__ import print_function
 
 import argparse
 import sys
+import time
 
 import gpib
 
 def ps_monitor_current(power_supply, output=sys.stdout, print_to_console=True):
-    """Monitor and save current until keyboard interrupt.
+    """Monitor and save power supply output current until keyboard interrupt.
 
     Data points (timestamp and current) are saved to power_supply.data, and
     dumped to the output file when keyboard interrupt is encountered. Current
@@ -22,14 +24,15 @@ def ps_monitor_current(power_supply, output=sys.stdout, print_to_console=True):
     True.
 
     Adjacent recordings will be seperated by at least
-    power_supply.sampling_interval. See gpib.PowerSupply.record_current
-    for details.
+    power_supply.sampling_interval (0.125 seconds by default). See
+    gpib.PowerSupply.record_current for details.
 
     Arguments:
     power_supply: gpib.PowerSupply instance
-    output: file object for writing output
+    output: file object for writing output; defaults to sys.stdout
     print_to_console: if True, print to stderr in addtion to saving; defaults to
                       True
+
     """
     while True:
         try:
@@ -44,6 +47,33 @@ def ps_monitor_current(power_supply, output=sys.stdout, print_to_console=True):
                              (data_point['timestamp'], data_point['current']))
             break
 
+def li_monitor_current(lock_in, sampling_rate, output=sys.stdout):
+    """Monitor and save lock-in amplifier data points until keyboard interrupt.
+
+    Use the lock-in's internal buffer. The buffer is refreshed after every 8000
+    data points. Data points are saved internally to the lock_in object, and
+    dumped to the output file when keyboard interrupt is encountered.
+
+    Arguments:
+    lock_in: gpib.LockIn instance; must be initialized with the
+             gpib.LockIn.initialize method
+    sampling_rate: sampling rate of the lock-in amplifier; should match the
+                   sampling rate passed to gpib.LockIn.initialize
+    output: file object for writing output; defaults to sys.stdout
+    """
+    while True:
+        try:
+            lock_in.start_storage()
+            time.sleep(8000 / sampling_rate)
+            lock_in.retrieve_storage()
+        except KeyboardInterrupt:
+            sys.stderr.write("\ninterrupted\n")
+            data = lock_in.data()
+            for data_point in data:
+                output.write("%.4f,%.4f\n" %
+                             (data_point['timestamp'], data_point['value']))
+            break
+
 def main():
     """CLI interface."""
     parser = argparse.ArgumentParser(description="GPIB intrument controller.")
@@ -54,18 +84,31 @@ def main():
                         help="output file; if not given, write to stdout")
     args = parser.parse_args()
     if args.action == 'monitor-power-supply':
-        ps = gpib.PowerSupply()
+        power_supply = gpib.PowerSupply()
         if args.file is None:
-            ps_monitor_current(ps)
+            ps_monitor_current(power_supply)
         else:
             try:
                 with open(args.file, 'w') as output:
-                    ps_monitor_current(ps, output)
+                    ps_monitor_current(power_supply, output)
             except (IOError, OSError) as err:
                 sys.stderr.write(type(err).__name__ + ": " + str(err) + "\n")
                 sys.stderr.write("error: invalid output file\n")
     elif args.action == 'monitor-lock-in':
-        pass #!
+        lock_in = gpib.LockIn()
+        lock_in.initialize()
+        if args.file is None:
+            li_monitor_current(lock_in, 8.0)
+        else:
+            try:
+                with open(args.file, 'w') as output:
+                    li_monitor_current(lock_in, 8.0, output)
+            except (IOError, OSError) as err:
+                sys.stderr.write(type(err).__name__ + ": " + str(err) + "\n")
+                sys.stderr.write("error: invalid output file\n")
+    else:
+        # placeholder for other possible actions
+        pass
 
 if __name__ == "__main__":
     main()
